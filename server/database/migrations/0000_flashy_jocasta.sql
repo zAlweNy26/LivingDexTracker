868ff07906f6ee17281a8091c111861f0094c2bf
Dany <zdanymc@gmail.com>
2025-06-06 08:40:11 +0200

    chore: add start migration

---
+++ server/database/migrations/0000_flashy_jocasta.sql
@@ -0,0 +1,48 @@
+CREATE TABLE `accounts` (
+	`id` text PRIMARY KEY NOT NULL,
+	`account_id` text NOT NULL,
+	`provider_id` text NOT NULL,
+	`user_id` text NOT NULL,
+	`access_token` text,
+	`refresh_token` text,
+	`id_token` text,
+	`access_token_expires_at` integer,
+	`refresh_token_expires_at` integer,
+	`scope` text,
+	`password` text,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`email_verified` integer NOT NULL,
+	`image` text,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+CREATE TABLE `verifications` (
+	`id` text PRIMARY KEY NOT NULL,
+	`identifier` text NOT NULL,
+	`value` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	`created_at` integer,
+	`updated_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `pokedex` (
+	`user_id` text,
+	`updated_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`ndex` text NOT NULL,
+	`form` integer DEFAULT 0 NOT NULL,
+	`shiny` integer DEFAULT false NOT NULL,
+	`value` integer DEFAULT false NOT NULL,
+	`game` integer,
+	PRIMARY KEY(`user_id`, `ndex`, `form`, `shiny`),
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
