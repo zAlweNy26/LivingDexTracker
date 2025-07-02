9e69fd709a44238803fa2c3e57b2f320ce79adb9
Dany <zdanymc@gmail.com>
2025-07-02 22:10:22 +0200

    chore: update seo

--- app/app.vue
+++ app/app.vue
@@ -2,13 +2,22 @@
 import * as locales from '@nuxt/ui/locale'
 
 const { locale, t } = useI18n()
-
+const route = useRoute()
+const error = useError()
 const title = useAppConfig().title
 
+const seoTitle = computed(() => {
+  const routeName = String(route.name).replace('games-', '')
+  if (error.value) return t('error.routes.title', [error.value.statusCode])
+  if (!routeName || routeName === 'index') return t('header.home')
+  if (routeName in otherGames) return otherGames[routeName as keyof typeof otherGames]
+  return t(`header.${routeName}`)
+})
+
 defineOgImageComponent('SeoImage', {
-  headline: t('header.home'),
+  headline: seoTitle.value,
   title,
-  description: t('home.description'),
+  description: t('seoDescription'),
 })
 </script>
 
